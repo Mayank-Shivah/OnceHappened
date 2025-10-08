2c8655e49cdfe5c0d06b04e441e2a1005329c7c4
Mayank-Shivah <mayank@shivahwebtech.com>
2025-10-08 17:00:03 +0530

    cancel page changes

--- src/pages/Subscription/Cancel.js
+++ src/pages/Subscription/Cancel.js
@@ -3,22 +3,18 @@ import Swal from "sweetalert2";
 import "./success-cancel.scss";
 
 export default function Cancel() {
-  useEffect(() => {
-    Swal.fire({
-      title: "❌ Payment Canceled",
-      text: "No worries — you can try again anytime.",
-      icon: "warning",
-      confirmButtonText: "Go Back",
-    }).then(() => {
-      window.location.href = "/";
-    });
-  }, []);
+  
 
   return (
-    <div className="success-cancel-container">
-      <h2 className="cancel">❌ Payment Canceled</h2>
+   <>
+   <div className="main-container-success">
+     <div className="success-cancel-container cancel-main-section">
+      <h2 className="cancel">Payment Canceled</h2>
       <p>No worries — you can try again anytime.</p>
       <a href="/" className="btn-back">Go Back</a>
     </div>
+   </div>
+   
+   </>
   );
 }
