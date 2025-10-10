6b31d83d16aa8be46e60d5491309d4f696fa6b6a
harpree-shivah <harpreet@shivawebtech.com>
2025-10-10 10:11:26 +0530

    10-10-2025 return-policy content add

--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -32,7 +32,7 @@ function About() {
                         {/* <div className="privacy-card">
                             <h1 >Privacy Policy <span> (Update Date:- 10/08/2025)</span></h1>
                         </div> */}
-                        <div className="policy-page" >
+                        <div className="policy-page about-text" >
                             <p>
                                 We are a personal experiences sharing platform where people post what they have read somewhere, remember about, have personally experienced, or somebody they know experienced, so that all readers learn a thing or two from it and enrich their wisdom which can help them in future interactions with the world.
                             </p>
